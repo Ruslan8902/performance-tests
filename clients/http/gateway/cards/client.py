c537169dc9579b84c8b71bddb99a666a796094fd
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-09-22 22:44:47 +0500

    7.4 Практикуемся в реализации HTTP API клиентов. Практика реализации HTTP API клиентов: /api/v1/operations

--- clients/http/gateway/cards/client.py
+++ clients/http/gateway/cards/client.py
@@ -4,9 +4,17 @@ from clients.http.client import HTTPClient
 from httpx import Response
 
 
-class IssueCardRequestDict(TypedDict):
+class IssueVirtualCardRequestDict(TypedDict):
     """
-    Структура данных для создания новой виртуальной/физической карты.
+    Структура данных для выпуска виртуальной карты.
+    """
+    userId: str
+    accountId: str
+
+
+class IssuePhysicalCardRequestDict(TypedDict):
+    """
+    Структура данных для выпуска физической карты.
     """
     userId: str
     accountId: str
@@ -17,20 +25,20 @@ class CardsGatewayHTTPClient(HTTPClient):
     Клиент для взаимодействия с /api/v1/cards сервиса http-gateway.
     """
 
-    def issue_virtual_card_api(self, request: IssueCardRequestDict) -> Response:
+    def issue_virtual_card_api(self, request: IssueVirtualCardRequestDict) -> Response:
         """
         Выпуск новой виртуальной карты.
 
         :param request: Словарь с данными для создания новой виртуальной карты.
         :return: Ответ от сервера (объект httpx.Response).
         """
-        return self.client.post(url="/api/v1/cards/issue-virtual-card", json=request)
+        return self.post(url="/api/v1/cards/issue-virtual-card", json=request)
 
-    def issue_physical_card_api(self, request: IssueCardRequestDict) -> Response:
+    def issue_physical_card_api(self, request: IssuePhysicalCardRequestDict) -> Response:
         """
         Выпуск новой физической карты.
 
         :param request: Словарь с данными для создания новой физической карты.
         :return: Ответ от сервера (объект httpx.Response).
         """
-        return self.client.post(url="/api/v1/cards/issue-physical-card", json=request)
+        return self.post(url="/api/v1/cards/issue-physical-card", json=request)
