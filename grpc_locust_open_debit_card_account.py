4f8501ec816249741ea705454336ebed6af255f1
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-10-02 23:04:28 +0500

    10.5 Использование gRPC API клиентов в нагрузочных сценариях. Практика: Применение gRPC API клиентов в нагрузочном тестировании

--- grpc_locust_open_debit_card_account.py
+++ grpc_locust_open_debit_card_account.py
@@ -16,9 +16,10 @@ class OpenDebitCardAccountScenarioUser(User):
 
     def on_start(self) -> None:
         """
-        Метод on_start вызывается один раз при запуске каждой сессии виртуального пользователя.
-        Здесь мы создаем нового пользователя, отправляя POST-запрос к /api/v1/users.
+        Метод, вызываемый при старте каждого виртуального пользователя.
+        Здесь происходит инициализация gRPC API клиентов и создание пользователя.
         """
+        # Инициализируем gRPC-клиентов, пригодный для Locust, с интерцептором метрик.
         self.users_gateway_client = build_users_gateway_locust_grpc_client(self.environment)
         self.account_gateway_client = build_accounts_gateway_locust_grpc_client(self.environment)
         self.create_user_response = self.users_gateway_client.create_user()
@@ -26,7 +27,7 @@ class OpenDebitCardAccountScenarioUser(User):
     @task
     def open_debit_card_account(self):
         """
-        Основная нагрузочная задача: открытие дебетового счета.
-        Здесь мы выполняем POST-запрос к /api/v1/accounts/open-debit-card-account.
+        Основная задача виртуального пользователя — открытие дебетового счета.
+        Метод будет многократно вызываться Locust-агентами.
         """
         self.account_gateway_client.open_debit_card_account(user_id=self.create_user_response.user.id)
