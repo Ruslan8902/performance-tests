be0d848b2b27691a2a1d3dc3374cac6e7894a28f
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-10-06 21:05:45 +0500

    10.10 Рефакторинг и улучшение нагрузочных тестов. Практика: Применение базового виртуального пользователя в нагрузочных сценариях

--- scenarios/http/gateway/get_accounts/scenario.py
+++ scenarios/http/gateway/get_accounts/scenario.py
@@ -3,6 +3,7 @@ from locust import User, between, task
 # Импортируем схемы ответов, чтобы типизировать shared state
 from clients.http.gateway.locust import GatewayHTTPTaskSet
 from clients.http.gateway.users.schema import CreateUserResponseSchema
+from tools.locust.user import LocustBaseUser
 
 
 class GetAccountsTaskSet(GatewayHTTPTaskSet):
@@ -51,10 +52,8 @@ class GetAccountsTaskSet(GatewayHTTPTaskSet):
         )
 
 
-class GetAccountsScenarioUser(User):
+class GetAccountsScenarioUser(LocustBaseUser):
     """
     Пользователь Locust, исполняющий сценарий получения счетов.
     """
-    host = "localhost"
     tasks = [GetAccountsTaskSet]
-    wait_time = between(1, 3)  # Имитируем паузы между выполнением сценариев
